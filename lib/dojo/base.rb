a26eef7f501242634a367f8012039153f3fb93a6
Benjamin Severac <bseverac@servebox.com>
2014-06-05 14:28:34 +0200

    bowling branch

--- lib/dojo/base.rb
+++ lib/dojo/base.rb
@@ -1,7 +1,70 @@
 module Dojo
-  class Base
-    def self.starting_dojo!
-      "starting dojo"
+  class Game
+
+    def initialize
+      @frames = [Frame.new]
+    end
+
+    def score
+      @frames.reduce(0){|sum, frame| sum + frame.score}
+    end
+
+    def record!(pins)
+      frame.add!(pins)
+    end
+
+    def frame
+      if @frames.last.finish?
+        @frames.last.next = add_frame
+      else
+        @frames.last
+      end
+    end
+
+    private
+    def add_frame
+      @frames << Frame.new
+      @frames.last
+    end
+  end
+
+  class Frame
+
+    def initialize
+      @pins = []
+    end
+    
+    def next= frame
+      @next = frame
+    end
+
+    def finish?
+      @pins.length == 2
+    end
+
+    def spare?
+      @pins[1]=='/'
+    end
+    
+    def pins(index)
+      @pins[index]
+    end
+
+    def spare_score
+      if @next
+        10 + @next.pins(0).to_i
+      else
+        0
+      end
+    end
+
+    def score
+      return spare_score if spare?
+      finish? ? @pins.reduce(0){|sum, pin| sum + pin.to_i } : 0
+    end
+
+    def add!(pins)
+      @pins << pins
     end
   end
 end
--- spec/dojo/base_spec.rb
+++ spec/dojo/base_spec.rb
@@ -1,13 +1,41 @@
 require 'spec_helper'
 
-describe Dojo::Base do
+describe Dojo::Game do
 
   before do
     @base = subject.new
   end
 
-  it 'must say we starting dojo' do
-    Dojo::Base.starting_dojo!.must_equal "starting dojo"
+  it 'initialize the score' do
+    @base.score.must_equal 0
+  end
+
+  it 'should not change score on first throw when hit 1' do
+    @base.record! '1'
+    @base.score.must_equal 0
+  end
+
+  it 'changes score to 2 when 1 pin hit twice' do
+    2.times do @base.record! '1' end
+    @base.score.must_equal 2
+  end
+
+  it 'doesnt change the score on a strike' do
+    @base.record! 'X'
+    @base.score.must_equal 0
+  end
+  
+  it 'doesnt change the score on a spare' do
+    @base.record! '1'
+    @base.record! '/'
+    @base.score.must_equal 0
+  end
+
+  it 'update score on first hit and spare before' do
+    @base.record! '1'
+    @base.record! '/'
+    @base.record! '1'
+    @base.score.must_equal 11
   end
 
 end
