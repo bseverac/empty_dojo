4ab02a693b61a4dec41f15aab6c13393f90773fd
Benjamin Severac <bseverac@servebox.com>
2014-08-04 16:49:26 +0200

    dojo : donut rover

--- lib/dojo/base.rb
+++ lib/dojo/base.rb
@@ -1,7 +1,119 @@
 module Dojo
-  class Base
-    def self.starting_dojo!
-      "starting dojo"
+  class Donut
+    attr_reader :dimension
+
+    def initialize(options)
+      @dimension = options
+      @obstacles = options[:obstacles] || []
+    end
+
+    def adjust(position)
+      {
+        x: position[:x] % @dimension[:x],
+        y: position[:y] % @dimension[:y]
+      }
+    end
+
+    def collide!(position)
+      raise CollisionError.new(position, "I have to stop there") if @obstacles.include?(position)
+      position
+    end
+  end
+
+  class CollisionError < RuntimeError
+    attr_reader :obstacle_position
+
+    def initialize(obstacle_position, msg)
+      super msg
+      @obstacle_position = obstacle_position
+    end
+  end
+
+  class Rover
+
+    attr_reader :position, :direction, :donut
+
+    COMMANDS={
+      'f' => :forward,
+      'b' => :backward,
+      'r' => :right,
+      'l' => :left
+    }
+
+    def initialize(options)
+      @navigator = Navigator.new(self)
+      @position = {x: options[:x], y: options[:y]}
+      @direction = options[:direction]
+      @donut = options[:donut]
+    end
+
+    def do!(*commands)
+      commands.each do |command|
+        @position,@direction  = @navigator.send(COMMANDS[command])
+      end
+    end
+
+    class Navigator
+      MOVES = {
+        north: {axis: :vertical, operator: :+},
+        east:  {axis: :horizontal, operator: :+},
+        south: {axis: :vertical, operator: :-},
+        west:  {axis: :horizontal, operator: :-}
+      }
+      DIRECTIONS = [:north, :east, :south, :west]
+      def initialize(rover)
+        @rover = rover
+      end
+
+      def right
+        rotate(:+)
+      end
+
+      def left
+        rotate(:-)
+      end
+
+      def forward
+        move = MOVES[@rover.direction]
+        send("#{move[:axis]}_move", move[:operator])
+      end
+
+      def backward
+        move = MOVES[@rover.direction]
+        send("#{move[:axis]}_move", reverse(move[:operator]))
+      end
+
+      def reverse(operator)
+        case operator
+        when :+
+          :-
+        when :-
+          :+
+        end
+      end
+
+      private
+      def rotate(operator)
+        next_index = (DIRECTIONS.index(@rover.direction).send(operator, 1)) % 4
+        return @rover.position, DIRECTIONS[next_index]
+      end
+
+      def vertical_move(operator)
+        move(:y, operator)
+      end
+
+      def horizontal_move(operator)
+        move(:x, operator)
+      end
+
+      def move(property, operator)
+        target_position = @rover.position.merge({
+          property => @rover.position[property].send(operator, 1)
+        })
+        return @rover.donut.collide!(
+          @rover.donut.adjust(target_position)
+        ), @rover.direction
+      end
     end
   end
 end
--- spec/dojo/base_spec.rb
+++ spec/dojo/base_spec.rb
@@ -1,13 +1,79 @@
 require 'spec_helper'
 
-describe Dojo::Base do
+describe Dojo::Rover do
 
-  before do
-    @base = subject.new
+  let :rover do
+    donut = Dojo::Donut.new(x: 100, y: 100)
+    subject.new(x: 10,y: 12,direction: :north, donut: donut)
   end
 
-  it 'must say we starting dojo' do
-    Dojo::Base.starting_dojo!.must_equal "starting dojo"
+  it 'must initialize rover' do
+    rover.position.must_equal({x:10, y:12})
+    rover.direction.must_equal :north
   end
 
+  it 'moves forward when receiving f' do
+    rover.do!('f')
+    rover.position.must_equal({x:10, y:13})
+  end
+
+  it 'draws a small square' do
+    rover.do!('f', 'r', 'f', 'r', 'f', 'r', 'f')
+    rover.position.must_equal({x:10, y:12})
+    rover.direction.must_equal :west
+  end
+
+  it 'draws a small square backwards' do
+    rover.do!('b', 'r', 'b', 'r', 'b', 'r', 'b')
+    rover.position.must_equal({x:10, y:12})
+    rover.direction.must_equal :west
+  end
+
+  it 'goes back to its starting point when receiving fb' do
+    rover.do!('f','b')
+    rover.position.must_equal({x:10, y:12})
+  end
+
+  it 'make a full clockwise turn' do
+    rover.do!('r')
+    rover.direction.must_equal :east
+    rover.do!('r')
+    rover.direction.must_equal :south
+    rover.do!('r')
+    rover.direction.must_equal :west
+    rover.do!('r')
+    rover.direction.must_equal :north
+  end
+
+  it 'make a full counter-clockwise turn' do
+    rover.do!('l')
+    rover.direction.must_equal :west
+    rover.do!('l')
+    rover.direction.must_equal :south
+    rover.do!('l')
+    rover.direction.must_equal :east
+    rover.do!('l')
+    rover.direction.must_equal :north
+  end
+
+  it 'cross the grid edge from the top and go back' do
+    donut = Dojo::Donut.new(x: 100, y: 100)
+    rover = Dojo::Rover.new(x: 99, y: 99,direction: :north, donut: donut)
+    rover.do!('f')
+    rover.position.must_equal({x: 99, y: 0})
+    rover.do!('b')
+    rover.position.must_equal({x: 99, y: 99})
+    rover.do!('r', 'f')
+    rover.position.must_equal({x: 0, y: 99})
+    rover.do!('b')
+    rover.position.must_equal({x: 99, y: 99})
+  end
+
+  it 'stop in front of a obstacle' do
+    donut = Dojo::Donut.new(x: 100, y: 100, obstacles: [{x: 1, y:1}])
+    rover = Dojo::Rover.new(x: 0, y: 0,direction: :north, donut: donut)
+    error = ->{ rover.do!('f','r','f') }.must_raise Dojo::CollisionError
+    rover.position.must_equal({x: 0, y: 1})
+    error.obstacle_position.must_equal({x: 1, y: 1})
+  end
 end
